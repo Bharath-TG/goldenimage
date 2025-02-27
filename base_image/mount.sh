f94b47832daa93712c286581fad3d5102596db9e
Bharath-TG <bharath.tg@twidpay.com>
2025-02-27 17:05:18 +0530

    Create mount.sh

---
+++ base_image/mount.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -e /dev/nvme1n1 ]; then
+
+  if ! blkid /dev/nvme1n1 | grep -q "TYPE=\"xfs\""; then
+    echo "The device /dev/nvme1n1 is not formatted with XFS. Formatting it now."
+    sudo mkfs.xfs /dev/nvme1n1
+  else
+    echo "The device /dev/nvme1n1 is already formatted with XFS."
+  fi
+
+  sudo mkdir -p /twid
+  sudo mount /dev/nvme1n1 /twid
+  UUID=$(blkid -s UUID -o value /dev/nvme1n1)
+  
+  echo "UUID=$UUID /twid xfs defaults 0 0" | sudo tee -a /etc/fstab
+
+else
+  echo "Device /dev/nvme1n1 does not exist."
+fi
+
